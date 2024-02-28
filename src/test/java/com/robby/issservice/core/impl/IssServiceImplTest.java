0fbfe49e74bb21ea30aeb25f0c07eff92bb4524c
Robby Llena <john.llena@maya.ph>
2024-02-28 11:17:50 +0800

    Add unit test for getLandmarks

--- src/test/java/com/robby/issservice/core/impl/IssServiceImplTest.java
+++ src/test/java/com/robby/issservice/core/impl/IssServiceImplTest.java
@@ -4,11 +4,15 @@ import com.robby.issservice.core.IssLocationService;
 import com.robby.issservice.core.IssService;
 import com.robby.issservice.core.LandmarkService;
 import com.robby.issservice.core.model.Coordinates;
+import com.robby.issservice.core.model.Landmark;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -36,5 +40,27 @@ class IssServiceImplTest {
         Coordinates currentLocation = service.getCurrentLocation();
 
         assertEquals(new Coordinates(1d, 1d), currentLocation);
+    }
+
+    @Test
+    void whenGetLandmarksThenReceiveLandmarks() {
+        when(landmarkService.getLandmarks(new Coordinates(1d, 1d))).thenReturn(
+                Collections.singletonList(Landmark.builder()
+                                .title("landmark")
+                                .latitude(1d)
+                                .longitude(1d)
+                                .country("PH")
+                        .build()));
+
+        List<Landmark> landmarks = service.getLandmarks(new Coordinates(1d, 1d));
+
+        assertEquals(
+                Collections.singletonList(Landmark.builder()
+                        .title("landmark")
+                        .latitude(1d)
+                        .longitude(1d)
+                        .country("PH")
+                        .build()),
+                landmarks);
     }
 }
